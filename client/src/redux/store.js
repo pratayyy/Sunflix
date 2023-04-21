57b9b4fd0417b3b2504f430f7ac9ef7ae41e914a
pratayyy <2028026@kiit.ac.in>
2023-04-21 13:13:17 +0530

    redux feature slice added

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,17 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import appStateSlice from "./features/appStateSlice";
-// import authModalSlice from "./features/authModalSlice";
-// import globalLoadingSlice from "./features/globalLoadingSlice";
-// import themeModeSlice from "./features/themeModeSlice";
+import appStateSlice from "./features/appStateSlice";
+import authModalSlice from "./features/authModalSlice";
+import globalLoadingSlice from "./features/globalLoadingSlice";
+import themeModeSlice from "./features/themeModeSlice";
 import userSlice from "./features/userSlice";
 
 const store = configureStore({
   reducer: {
     user: userSlice,
-    // themeMode: themeModeSlice,
-    // authModal: authModalSlice,
-    // globalLoading: globalLoadingSlice,
-    // appState: appStateSlice
+    themeMode: themeModeSlice,
+    authModal: authModalSlice,
+    globalLoading: globalLoadingSlice,
+    appState: appStateSlice,
   },
 });
 
